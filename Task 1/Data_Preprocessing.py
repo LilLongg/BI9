00a12456cb97b77aecc5df77b4b63b34703db189
LilLongg <glivingstone70@gmail.com>
2025-06-14 18:07:44 +0700

    Update customer dataset

--- Task 1/Data_Preprocessing.py
+++ Task 1/Data_Preprocessing.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.13.15"
-app = marimo.App(width="medium", auto_download=["ipynb"])
+app = marimo.App(width="medium")
 
 
 @app.cell
@@ -23,7 +23,6 @@ def _():
     import polars as pl
     from pathlib import Path
     import re
-
     return Path, mo, pl
 
 
@@ -48,54 +47,371 @@ def _(mo):
 
 @app.cell
 def _(mo):
-    mo.md(r"""### Brandhealth Dataset""")
+    mo.md(r"""### Customer Dataset""")
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""#### Read the dataset""")
+    mo.md(r"""Read the dataset""")
     return
 
 
 @app.cell
 def _(DATA_DIR, pl):
-    raw_brand_health_df = pl.read_csv(
-        DATA_DIR / "Brandhealth.csv",
-        separator=";",
-        schema_overrides={
-            "PPA": float,
-            "Fre#visit": int,
-            "NPS#P3M": int,
-            "Spending": int,
-            "Spending_use": int,
-        },
-    ).unique()
-    raw_brand_health_df
-    return (raw_brand_health_df,)
+    raw_customer_df = (
+        pl.read_csv(DATA_DIR / "SA#var.csv", separator=";")
+        .unique()
+        .drop(["Col", "MPI_Mean_Use", "MPI#2"])
+    )
+    raw_customer_df
+    return (raw_customer_df,)
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""#### Check if each ID applies for one year and one city""")
+    mo.md(r"""#### Check if the ID is unique""")
     return
 
 
 @app.cell
-def _(raw_brand_health_df):
-    (
-        raw_brand_health_df.unique("ID").height,
-        raw_brand_health_df.unique(["ID", "Year", "City"]).height,
-    )
+def _(raw_customer_df):
+    raw_customer_df["ID"].unique().len(), raw_customer_df.height
     return
 
 
 @app.cell
 def _(mo):
     mo.md(
-        r"""The number of unique rows with `ID`, and the tuple (`ID`, `Year`, `City`) is the same, suggesting that `Year` and `City` can be derived from a unique `ID`."""
+        r"""The ID is unique, indicating that each ID is applied for one group in a city in a year."""
     )
     return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#### Impute missing values""")
+    return
+
+
+@app.cell
+def _(raw_customer_df):
+    raw_customer_df.null_count()
+    return
+
+
+@app.cell
+def _(mo, raw_customer_df):
+    mo.md(
+        rf"""For attributes `Age` and `Group_size`, since the missing values size is so small ({100 * raw_customer_df.null_count()[0, "Age"] / raw_customer_df.height:.3f}% and {100 * raw_customer_df.null_count()[0, "Group_size"] / raw_customer_df.height:.3f}%), we can impute using `mean`."""
+    )
+    return
+
+
+@app.cell
+def _(pl, raw_customer_df):
+    customer_df = raw_customer_df.with_columns(
+        pl.col(["Age", "Group_size"]).fill_null(strategy="mean")
+    )
+    customer_df["Age", "Group_size"].null_count()
+    return (customer_df,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""After imputing `Age`, we will impute the `Age group`:""")
+    return
+
+
+@app.cell
+def _(customer_df):
+    customer_df["Age#group", "Age#Group#2"].unique().sort("*")
+    return
+
+
+@app.cell
+def _():
+    def map_age_group(age: int) -> str:
+        if age < 20:
+            return "16 - 19"
+        if age < 30:
+            return "20 - 29"
+        if age < 40:
+            return "30 - 39"
+        return "40 - 60"
+
+
+    def map_age_group_2(age: int) -> str:
+        if age < 20:
+            return "16 - 19 y.o."
+        if age < 25:
+            return "20 - 24 y.o."
+        if age < 30:
+            return "25 - 29 y.o."
+        if age < 35:
+            return "30 - 34 y.o."
+        if age < 40:
+            return "35 - 39 y.o."
+        if age < 45:
+            return "40 - 44 y.o."
+        return "45+ y.o."
+    return map_age_group, map_age_group_2
+
+
+@app.cell
+def _(customer_df, map_age_group, map_age_group_2, pl):
+    AGE_GROUP_MAPS = {"Age#group": map_age_group, "Age#Group#2": map_age_group_2}
+
+    age_imputed_df = customer_df.with_columns(
+        [
+            pl.when(pl.col(key).is_null())
+            .then(pl.col("Age").map_elements(func, return_dtype=str))
+            .otherwise(pl.col(key))
+            .alias(key)
+            for key, func in AGE_GROUP_MAPS.items()
+        ]
+    )
+    age_imputed_df[*AGE_GROUP_MAPS.keys()].null_count()
+    return (age_imputed_df,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    For `MPI`, we can take the mean from the same group.
+
+    The group includes:
+
+    - `City`
+    - `Gender`
+    - `Age group`
+    - `Occupation group`
+
+    `Year` is not included since there is no information of the year 2017.
+    """
+    )
+    return
+
+
+@app.cell
+def _(age_imputed_df, pl):
+    MPI_IMPUTE_GROUPS = [
+        "City",
+        "Gender",
+        "Age#Group#2",
+        "Occupation#group",
+    ]
+
+    customer_mpi_non_null = (
+        age_imputed_df.filter(~pl.col("MPI#Mean").is_null())
+        .group_by(MPI_IMPUTE_GROUPS)
+        .agg(
+            [
+                pl.col("MPI#Mean").mean().cast(int),
+                pl.col("City").len().alias("Count"),
+            ]
+        )
+    )
+    customer_mpi_non_null
+    return MPI_IMPUTE_GROUPS, customer_mpi_non_null
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""We use these groups to impute.""")
+    return
+
+
+@app.cell
+def _(MPI_IMPUTE_GROUPS, age_imputed_df, customer_mpi_non_null, pl):
+    mpi_imputed_df = (
+        age_imputed_df.join(
+            customer_mpi_non_null, how="left", on=MPI_IMPUTE_GROUPS
+        )
+        .with_columns(
+            pl.when(pl.col("MPI#Mean").is_null())
+            .then(pl.col("MPI#Mean_right"))
+            .otherwise(pl.col("MPI#Mean"))
+            .alias("MPI#Mean")
+        )
+        .drop(["MPI#Mean_right", "Count"])
+    )
+    mpi_imputed_df["MPI#Mean"].null_count()
+    return (mpi_imputed_df,)
+
+
+@app.cell
+def _(mo, mpi_imputed_df):
+    mo.md(
+        rf"""There are still missing values, due to some outliers that do not belong to any group. Since the proportion is small ({100 * mpi_imputed_df.null_count()[0, "MPI#Mean"] / mpi_imputed_df.height:.3f}%), we can use mean to impute the rest."""
+    )
+    return
+
+
+@app.cell
+def _(mpi_imputed_df, pl):
+    imputed_customer_df = mpi_imputed_df.with_columns(
+        pl.col("MPI#Mean").fill_null(strategy="mean")
+    )
+    imputed_customer_df["MPI#Mean"].null_count()
+    return (imputed_customer_df,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""With that, we can also impute `MPI Group`""")
+    return
+
+
+@app.cell
+def _(imputed_customer_df):
+    (
+        imputed_customer_df["MPI"].unique().sort(),
+        imputed_customer_df["MPI#detail"].unique().sort(),
+    )
+    return
+
+
+@app.cell
+def _():
+    def map_mpi_group(mpi: int) -> str:
+        if mpi < 4500:
+            return "Under VND 4.5m"
+        if mpi < 9000:
+            return "VND 4.5m - VND 8.9m"
+        if mpi < 15000:
+            return "VND 9m - VND 14.9m"
+        if mpi < 25000:
+            return "VND 15m - VND 24.9m"
+        return "VND 25m+"
+
+
+    def map_mpi_detail(mpi: int) -> str:
+        if mpi < 3000:
+            return "Under 3 millions VND"
+        if mpi < 4500:
+            return "From 3 millions to 4.49 millions VND"
+        if mpi < 6500:
+            return "From 4 millions to 6.49 millions VND"
+        if mpi < 7500:
+            return "From 6.5 millions to 7.49 millions VND"
+        if mpi < 9000:
+            return "From 7.5 millions to 8.99 millions VND"
+        if mpi < 12000:
+            return "From 9 millions to 11.99 millions VND"
+        if mpi < 15000:
+            return "From 12 millions to 14.99 millions VND"
+        if mpi < 20000:
+            return "From 15 millions to 19.99 millions VND"
+        if mpi < 25000:
+            return "From 20 millions to 24.99 millions VND"
+        if mpi < 30000:
+            return "From 25 millions to 29.99 millions VND"
+        if mpi < 45000:
+            return "From 30 millions to 44.99 millions VND"
+        if mpi < 75000:
+            return "From 45 millions to 74.99 millions VND"
+        return "From 75 million to VND 149.99 million VND"
+    return map_mpi_detail, map_mpi_group
+
+
+@app.cell
+def _(imputed_customer_df, map_mpi_detail, map_mpi_group, pl):
+    MPI_GROUP_MAPS = {"MPI": map_mpi_group, "MPI#detail": map_mpi_detail}
+
+
+    mpi_group_imputed_df = imputed_customer_df.with_columns(
+        [
+            pl.when(pl.col(key).is_null())
+            .then(pl.col("Age").map_elements(func, return_dtype=str))
+            .otherwise(pl.col(key))
+            .alias(key)
+            for key, func in MPI_GROUP_MAPS.items()
+        ]
+    )
+    mpi_group_imputed_df[*MPI_GROUP_MAPS.keys()].null_count()
+    return (mpi_group_imputed_df,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""Finally, we impute `BUMO_previous`.""")
+    return
+
+
+@app.cell
+def _(mpi_group_imputed_df):
+    mpi_group_imputed_df["BUMO_Previous"].unique().sort()
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""There is a value of `Don't have nay brands` which we can use to impute."""
+    )
+    return
+
+
+@app.cell
+def _(mpi_group_imputed_df, pl):
+    customer_cleaned_df = mpi_group_imputed_df.fill_null(
+        pl.lit("Don't have any brands")
+    )
+    customer_cleaned_df
+    return (customer_cleaned_df,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#### Save the result""")
+    return
+
+
+@app.cell
+def _(OUTPUT_DIR, customer_cleaned_df):
+    customer_cleaned_df.sort("*").write_csv(OUTPUT_DIR / "customer.csv")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Brandhealth Dataset""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    #### Read the dataset
+
+    - We drop `City` and `Year` since it is already defined on the customer dataset.
+    """
+    )
+    return
+
+
+@app.cell
+def _(DATA_DIR, pl):
+    raw_brand_health_df = (
+        pl.read_csv(
+            DATA_DIR / "Brandhealth.csv",
+            separator=";",
+            schema_overrides={
+                "PPA": float,
+                "Fre#visit": int,
+                "NPS#P3M": int,
+                "Spending": int,
+                "Spending_use": int,
+            },
+        )
+        .drop(["City", "Year"])
+        .unique()
+    )
+    raw_brand_health_df
+    return (raw_brand_health_df,)
 
 
 @app.cell
@@ -118,19 +434,13 @@ def _(pl, raw_brand_health_df):
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-    There are no results. Indicating that they are the same. Therefore we will remove $Spending_use$.
-
-    We will also remove `City` and `Year` for obvious reasons.
-    """
-    )
+    mo.md(r"""There are no results. Indicating that they are the same. Therefore we will remove $Spending_use$.""")
     return
 
 
 @app.cell
 def _(raw_brand_health_df):
-    brand_health_df = raw_brand_health_df.drop(["City", "Year", "Spending_use"])
+    brand_health_df = raw_brand_health_df.drop("Spending_use")
     return (brand_health_df,)
 
 
@@ -154,9 +464,7 @@ def _(mo):
 
 @app.cell
 def _(brand_health_df, mo):
-    mo.md(
-        rf"""For `Comprehension`, since there is no way to deduce its value, and it takes up a large proportion of the dataset ({100 * brand_health_df["Comprehension"].null_count() / brand_health_df.height:.2f}%), we can consider droping the column."""
-    )
+    mo.md(rf"""For `Comprehension`, since there is no way to deduce its value, and it takes up a large proportion of the dataset ({100 * brand_health_df["Comprehension"].null_count() / brand_health_df.height:.2f}%), we can consider dropping the column.""")
     return
 
 
@@ -223,13 +531,13 @@ def _():
             return "Seg.03 - Premium (VND 60K - VND 99K)"
         return "Seg.04 - Super Premium (VND 100K+)"
 
+
     def nps_map(nps: int) -> str:
         if nps < 7:
             return "Detractor"
         if nps < 9:
             return "Passive"
         return "Promoter"
-
     return nps_map, segmenatation_map
 
 
@@ -409,7 +717,9 @@ def _(brand_health_df_6, pl):
     brand_health_df_7 = brand_health_df_6.filter(
         (pl.col("Awareness") == True) | (pl.col("Trial") == False)
     )
-    brand_health_df_6.filter((pl.col("Awareness") == False) & (pl.col("Trial") == True))
+    brand_health_df_6.filter(
+        (pl.col("Awareness") == False) & (pl.col("Trial") == True)
+    )
     return (brand_health_df_7,)
 
 
@@ -451,7 +761,9 @@ def _(brand_health_df_8, pl):
     brand_health_df_9 = brand_health_df_8.filter(
         (pl.col("Fre#visit") == 0) ^ (pl.col("Spending") != 0)
     )
-    brand_health_df_8.filter((pl.col("Fre#visit") != 0) ^ (pl.col("Spending") != 0))
+    brand_health_df_8.filter(
+        (pl.col("Fre#visit") != 0) ^ (pl.col("Spending") != 0)
+    )
     return (brand_health_df_9,)
 
 
@@ -502,7 +814,7 @@ def _(mo):
 
 @app.cell
 def _(OUTPUT_DIR, brand_health_df_9):
-    brand_health_df_9.write_csv(OUTPUT_DIR / "Brand Health.csv")
+    brand_health_df_9.sort("*").write_csv(OUTPUT_DIR / "Brand Health.csv")
     return
 
 
